99766a49ca754465f9e33aee76d0fc05888ea3cb
MisterIB <163545760+MisterIB@users.noreply.github.com>
2024-11-24 01:21:54 +0700

    Update server.cpp

--- server/server.cpp
+++ server/server.cpp
@@ -3,15 +3,33 @@
 #include <unistd.h>
 #include <thread>
 
+#include <pqxx/pqxx>
+#include <fstream>
+
 using namespace std;
 
 void helloFunc() {
-    cout << "Hello world!!!" << endl;
+    ifstream conf("/configuration/configuration.json");
+    if (conf.is_open()) {
+        string str;
+        cout << "start" << endl;
+        while (conf >> str) {
+            cout << str;
+        }
+        cout << "end" << endl;
+    }
+    conf.close();
 }
 
 int main() {
     try {
 	    setlocale(LC_ALL, "RUSSIAN");
+        cout << "start listen sql" << endl;
+        pqxx::connection c("user=tester password=testPassword1 host=172.16.1.4 port=5432 dbname=tester target_session_attrs=read-write");
+        pqxx::work w(c);
+        pqxx::row r = w.exec1("SELECT 1");
+        w.commit();
+        std::cout << r[0].as<int>() << std::endl;
 
         int32_t serverSocket = socket(AF_INET, SOCK_STREAM, 0);
         if (serverSocket == 0) throw runtime_error("Error create socket");
