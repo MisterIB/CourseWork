53c178d467f434e82dfce7013990f594656b7c1d
MisterIB <163545760+MisterIB@users.noreply.github.com>
2024-12-10 00:10:48 +0700

    Create Configuration.h

---
+++ server/Configuration.h
@@ -0,0 +1,128 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Configuration {
+public:
+    string dataBaseName;
+    vector<string> tableNames;
+    map<string, vector<string>> columnNames;
+    string POSTGRES_USER;
+    string POSTGRES_PASSWORD;
+    string POSTGRES_PORT;
+    string POSTGRES_HOST;
+
+
+    Configuration(): dataBaseName("") {}
+
+    void readConfiguration(const string& pathToFile) {
+        ifstream confFile(pathToFile);
+        if (confFile.is_open()) {
+            dataBaseName = readDataBaseName(confFile);
+            readTableNames(confFile);
+            readPostgresConf(confFile);
+        }
+        confFile.close();
+    }   
+
+private:
+    string removeQuotes(const string& inputStr) {
+        string modifiedStr = inputStr;
+        if (modifiedStr[0] == '\"') modifiedStr.erase(0, 1);
+        if (modifiedStr[modifiedStr.size() - 1] == '\"') modifiedStr.erase(modifiedStr.size() - 1, 1);
+        return modifiedStr;
+    }
+
+    string removeColon(const string& inputStr) {
+        string modifiedStr = inputStr;
+        if (modifiedStr[modifiedStr.size() - 1] == ':') modifiedStr.erase(modifiedStr.size() - 1, 1);
+        return modifiedStr;
+    }
+
+    string removeSquareBrackets(const string& inputStr) {
+        string modifiedStr = inputStr;
+        if (modifiedStr[0] == '[') modifiedStr.erase(0, 1);
+        if (modifiedStr[modifiedStr.size() - 1] == ']') modifiedStr.erase(modifiedStr.size() - 1, 1);
+        return modifiedStr;
+    }
+
+    string removeComma(const string& inputStr) {
+        string modifiedStr = inputStr;
+        if (modifiedStr[modifiedStr.size() - 1] == ',') modifiedStr.erase(modifiedStr.size() - 1, 1);
+        return modifiedStr;
+    }
+
+    void readPostgresConf(ifstream& confFile) {
+        string inputStr;
+        while (confFile >> inputStr) {
+            string prevInputStr = inputStr;
+            confFile >> inputStr;
+            inputStr = removeComma(inputStr);
+            inputStr = removeQuotes(inputStr);
+            if (prevInputStr == "\"POSTGRES_USER\":") POSTGRES_USER = inputStr;
+            if (prevInputStr == "\"POSTGRES_PASSWORD\":") POSTGRES_PASSWORD = inputStr;
+            if (prevInputStr == "\"POSTGRES_PORT\":") POSTGRES_PORT = inputStr;
+            if (prevInputStr == "\"POSTGRES_HOST\":") POSTGRES_HOST = inputStr;
+        }
+    }
+
+    void readColumnNames(ifstream& confFile, const string& tableName) {
+        string inputStr;
+        getline(confFile, inputStr);
+        istringstream inputStrStream(inputStr);
+        string columnName;
+        while (inputStrStream >> columnName) {
+            columnName = removeComma(columnName);
+            columnName = removeSquareBrackets(columnName);
+            columnName = removeQuotes(columnName);
+            columnNames[tableName].push_back(columnName);
+        }
+    }
+
+    void readTableNames(ifstream& confFile) {
+        string inputStr;
+        confFile >> inputStr;
+        if (inputStr != "\"structure\":") throw runtime_error("Incorrect syntax in the JSON file");
+        confFile >> inputStr;
+        if (inputStr != "{") throw runtime_error("Incorrect syntax in the JSON file");
+        while (inputStr != "}" or inputStr != "},") {
+            confFile >> inputStr;
+            if (inputStr == "}" or inputStr == "},") break;
+            inputStr = removeColon(inputStr);
+            inputStr = removeQuotes(inputStr);
+            tableNames.push_back(inputStr);
+            readColumnNames(confFile, inputStr);
+        }
+    }
+
+    string readDataBaseName(ifstream& confFile) {
+        string inputStr;
+        confFile >> inputStr;
+        if (inputStr != "{") throw runtime_error("Incorrect syntax in the JSON file");
+        confFile >> inputStr;
+        if (inputStr == "\"name\":") confFile >> inputStr;
+        inputStr = removeComma(inputStr);
+        inputStr = removeQuotes(inputStr);
+        return inputStr;
+    }
+public:
+    void print() {
+        cout << dataBaseName << endl;
+        for (string str: tableNames) {
+            cout << str << " ";
+        }
+        cout << endl;
+        for (string str: tableNames) {
+            for (string strMp: columnNames[str]) {
+                cout << strMp << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    friend class UserInterface;
+};
