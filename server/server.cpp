3c321adc87286509d54b82d2295597e86c334568
MisterIB <163545760+MisterIB@users.noreply.github.com>
2024-12-11 01:11:34 +0700

    Update server.cpp

--- server/server.cpp
+++ server/server.cpp
@@ -1,6 +1,7 @@
-//Подключение сервера и клиента через ip
-//При поиске игрока выводить что нет игрока если нет
-//Удалить капс у лога
+//Работа с файлами исправить добавление последней строки /n за /n? 
+//Картинку хоккеиста побольше
+//Сделать чтобы всегда выводилось имя и фамилия
+//Редакция игрока
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -10,23 +11,14 @@
 #include <thread>
 #include <bits/stdc++.h>
 #include <pqxx/pqxx>
-#include <chrono>
-#include <ctime>
 
 #include "User.h"
 #include "Configuration.h"
 
 using namespace std;
 
-void createLog(string message) {
-    auto tm = chrono::system_clock::now();
-    time_t end_time = chrono::system_clock::to_time_t(tm);
-    //cout << ctime(&end_time) << " LOG: " << message << endl;
-    cout << " LOG: " << message << endl;
-}
-
 string startMenu() {
-    string message = "menu\nPlease enter your username";
+    string message = "menu\nAuthorization\nPlease enter your username and click \"OK\"\nTo log in without registration, write \"Guest\" and click \"OK\"";
     return message;
 }
 
@@ -47,30 +39,27 @@ string createLineToSend(pqxx::result& res) {
     string result = "";
     for (auto r: res) {
         for (auto f: r){
-            result += f.as<string>() + " ";
+            result += f.as<string>() + "\t";
         }
         result += '\n';
     }
     return result;
 }
 
-string SendRequestInDB(const string& requestInDB, Configuration& configuration) {
-    if (requestInDB == "") return "";
+string SendRequestInDB(string& requestInDB, Configuration& configuration) {
+    if (requestInDB == "") return "*";
+    if (requestInDB == "!") return "Incorrect request";
+    if (requestInDB[0] == '#') return requestInDB.erase(0, 1);
     string newRequestInDB = requestInDB + ";";
     pqxx::connection c("user=" + configuration.POSTGRES_USER + " password=" + configuration.POSTGRES_PASSWORD + " host=" + configuration.POSTGRES_HOST + " port=" + configuration.POSTGRES_PORT + " dbname=" + configuration.POSTGRES_USER + " target_session_attrs=read-write");
     pqxx::work db(c);
     pqxx::result res = db.exec(newRequestInDB);
+    createLog("A request has been sent to the database: " + newRequestInDB);
     db.commit();
     string result;
     result = createLineToSend(res);
     c.close();
     return result;
-}
-
-void disconnectClient(int32_t clientSocket) {
-    string message = "You are disconnected from the server";
-    send(clientSocket, message.c_str(), message.length(), 0);
-    close(clientSocket);
 }
 
 void processingRequests(int32_t clientSocket, unique_ptr<UserInterface>& user, Configuration& configuration) {
@@ -80,41 +69,33 @@ void processingRequests(int32_t clientSocket, unique_ptr<UserInterface>& user, C
         string request = readClientsRequest(clientSocket);
         int32_t inputData = stoi(request);
         string requestInDB;
-        int32_t amountOfColumns;
-        if (inputData == 1) requestInDB = user->printingTableData(configuration, amountOfColumns);
-        else if (inputData == 2) requestInDB = user->printStatOfTeamsPlayers(configuration);
-        else if (inputData == 3) requestInDB = user->printingTheBestPlayers(configuration);
-        else if (inputData == 4) requestInDB = user->playerSearch(configuration);
-        else if (inputData == 5) requestInDB = user->printStatOfTeamsPlayers(configuration);
-        else if (inputData == 7) {
-            disconnectClient(clientSocket);
-            return;
-        }
-        /*else if (inputData == 6)*/
+        string columnsForOutput = "";
+        if (inputData == 1) requestInDB = user->printingTableData(configuration, columnsForOutput);
+        else if (inputData == 2) requestInDB = user->printStatOfTeamsPlayers(configuration, columnsForOutput);
+        else if (inputData == 3) requestInDB = user->printingTheBestPlayers(configuration, columnsForOutput);
+        else if (inputData == 4) requestInDB = user->playerSearch(configuration, columnsForOutput);
+        else if (inputData == 5) requestInDB = user->changeTableData(configuration);
+        else if (inputData == 6) requestInDB = user ->registration(configuration);
         else {
             disconnectClient(clientSocket);
             return;
         }
         message = SendRequestInDB(requestInDB, configuration);
-        createLog("A request has been sent to the database: " + requestInDB);
-        if (message == "") {
-            close(clientSocket);
+        if (message == "*") {
+            disconnectClient(clientSocket);
             return;
         }
-        message += "\nTo send the next request, write \"Next\" and click \"OK\"";
-        send(clientSocket, message.c_str(), message.length(), 0);
+        string fullMessage = columnsForOutput + message;
+        if (message == "" and inputData != 6) fullMessage = "Nothing was found";
+        if (message == "" and inputData == 6) fullMessage = "menu\nYou are registered";
+        fullMessage += "\nTo send the next request, write \"Next\" and click \"OK\"";
+        send(clientSocket, fullMessage.c_str(), fullMessage.length(), 0);
         request = readClientsRequest(clientSocket);
         if (request != "Next") {
             disconnectClient(clientSocket);
             return;
         }
     }
-}
-
-int64_t hashFunction(const string& password) {
-    hash<string> hash;
-    string salt = "salt";
-    return hash(password + salt);
 }
 
 bool passwordVerification(const string& password, const string& username, Configuration& configuration) {
@@ -141,7 +122,7 @@ bool checkAccessRights(const string& username, Configuration& configuration) {
 
 void userAuthorization(int32_t clientSocket, Configuration& configuration) {
     string response;
-    response = readClientsRequest(clientSocket);// изменить
+    response = readClientsRequest(clientSocket);
     response = startMenu();
     send(clientSocket, response.c_str(), response.length(), 0);
     string userName = readClientsRequest(clientSocket);
@@ -151,16 +132,19 @@ void userAuthorization(int32_t clientSocket, Configuration& configuration) {
         processingRequests(clientSocket, user, configuration);
     }
     else {
-        response = "Enter the password (You have 3 attempts)";
+        response = "menu\nEnter the password (You have 3 attempts)";
         send(clientSocket, response.c_str(), response.length(), 0);
         int32_t amountOfAttempts = 3;
         while (amountOfAttempts != 0) {
             string passsword = readClientsRequest(clientSocket);
             if (passwordVerification(passsword, userName, configuration)) break;
             amountOfAttempts--;
+            response = "menu\nThe password was entered incorrectly\nPlease try again\nThere are still attempts left: " + to_string(amountOfAttempts);
+            send(clientSocket, response.c_str(), response.length(), 0);
         }
         if (amountOfAttempts == 0) {
-            throw runtime_error("The attempts are over");
+            createLog("The attempts are over: " + userName);
+            disconnectClient(clientSocket);
         }
         UserInterface* user = new GuestUser(userName, clientSocket);
         if (checkAccessRights(userName, configuration)) {
